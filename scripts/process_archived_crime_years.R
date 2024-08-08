6b2947408c63edb8a687081fc19244f6eb11241a
maiarosenfeld <83026850+maiarosenfeld@users.noreply.github.com>
2024-08-08 12:10:49 -0400

    Archive 2022 crime to reduce file size of new crime pull

--- scripts/process_archived_crime_years.R
+++ scripts/process_archived_crime_years.R
@@ -8,6 +8,7 @@ library(readxl)
 philly_crime19 <- read_csv("data/source/annual/phillycrime19.csv") %>% janitor::clean_names()
 philly_crime20 <- read_csv("data/source/annual/phillycrime20.csv") %>% janitor::clean_names()
 philly_crime21 <- read_csv("data/source/annual/phillycrime21.csv") %>% janitor::clean_names()
+philly_crime22 <- read_csv("data/source/annual/phillycrime22.csv") %>% janitor::clean_names()
 
-past_philly_crime <- rbind(philly_crime19,philly_crime20,philly_crime21)
+past_philly_crime <- rbind(philly_crime19,philly_crime20,philly_crime21,philly_crime22)
 saveRDS(past_philly_crime,"data/source/annual/past_philly_crime.RDS")
